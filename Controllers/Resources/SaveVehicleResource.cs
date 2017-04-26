443825f3c0a5e5a90b95a3f189af823d0243588b
PongsapakKan <pongsapakkanchitwaranont@gmail.com>
2017-04-27 00:35:30 +0700

    Update

---
+++ Controllers/Resources/SaveVehicleResource.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace vega.Controllers.Resources
+{
+
+    public class SaveVehicleResource
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ModelId { get; set; }
+
+        [Required]
+        public bool IsRegistered { get; set; }
+
+        [Required]
+        public ContactResource Contact { get; set; }
+        public ICollection<int> Features { get; set; }
+
+        public SaveVehicleResource()
+        {
+            Features = new Collection<int>();
+        }
+    }
+}
